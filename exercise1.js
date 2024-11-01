5f9c71f2aedb1b3cba274d2acf33f218e39bbb6c
riddsep <ridoseptiawan00@gmail.com>
2024-11-01 14:56:03 +0700

    finish: exercise 1

---
+++ exercise1.js
@@ -0,0 +1,10 @@
+const markWeight = 95;
+const markHeight = 188;
+
+const jhonWeight = 85;
+const jhonHeight = 176;
+
+const markBMI = (markWeight / markHeight) * markHeight;
+const jhonBMI = (jhonWeight / jhonHeight) * jhonHeight;
+
+console.log(markBMI, jhonBMI);
